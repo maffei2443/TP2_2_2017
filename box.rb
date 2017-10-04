5f8deb2895ac007bdbdc77fff8c33ddeaf76f91d
maffei2443 <leoitu22hotmail.com@gmail.com>
2017-10-03 23:24:50 -0300

    Branch conforme olicitacao ;)

--- box.rb
+++ box.rb
@@ -1,25 +1,25 @@
-class Hitbox
+class Hitbox # Define uma área tridimensional (limitada em x e y a retangulos e sem limitaçoes em z) onde está presente um objeto do jogo
   def initialize(x, y, zlist, width, length)
-    @width = width #largura
-    @length = length #comprimento
-    @x = x #ponto inicial x
-    @y = y #ponto inicial y
-    @zlist = zlist #alturas ocupadas pelo objeto
+    @width = width
+    @length = length
+    @x = x # Ponto inicial x
+    @y = y # Ponto inicial y
+    @zlist = zlist # Alturas ocupadas pelo objeto
   end
 
-  attr_reader   :width, :length, :zlist #zlist não é atribute acessor por que é um array, não precisa ser acessor pra alterar os elementos dentro do array
-  attr_accessor :x, :y #precisa ser acessado para movimentar a hitbox
+  attr_reader   :width, :length, :zlist # Zlist não é atribute acessor por que é um array, não precisa ser acessor pra alterar os elementos dentro do array
+  attr_accessor :x, :y # Precisa ser acessado para movimentar a hitbox
 
-  def check_hit(x_hit, y_hit, zlist_hit, width_hit, ln_hit) #checa se a hitbox tem overlap com outra hitbox, a partir dos elementos internos da outra
+  def check_hit(box) # Checa se a hitbox tem overlap com outra hitbox
     heightimp = false
-    @zlist.each do |heightLevel| #checa impacto de altura, se não tem impacto de altura, um objeto no maximo passa por cima do outro sem colidir
-      zlist_hit.each do |hitheight|
-        if (heightLevel == hitheight) #qualquer nivel de altura ser igual a qualquer nivel de altura da outra hitbox é o bastante pra ter impacto de altura
+    @zlist.each do |heightLevel| # Checa impacto de altura, se não tem impacto de altura, um objeto no maximo passa por cima do outro sem colidir
+      box.zlist.each do |hitheight|
+        if (heightLevel == hitheight) # Qualquer nivel de altura ser igual a qualquer nivel de altura da outra hitbox é o bastante pra ter impacto de altura
           heightimp = true
         end
       end
     end
-    if (((@x > x_hit + width_hit or x_hit > @x + @width) or (@y > y_hit + ln_hit or y_hit > @y + @length)) or (heightimp == false)) #se não tem overlap bidimensional ou não há impacto de altura, não tem colisão
+    if (((@x > box.x + box.width or box.x > @x + @width) or (@y > box.y + box.length or box.y > @y + @length)) or (heightimp == false)) # Se não tem overlap bidimensional ou não há impacto de altura, não tem colisão
       return false
     else
       return true
--- gameObject.rb
+++ gameObject.rb
@@ -2,7 +2,7 @@ require 'gosu'
 require './sprite'
 require './box'
 
-class GameObject #qualquer objeto que irá existir precisa desses elementos de classe
+class GameObject # Qualquer objeto que irá existir precisa desses elementos de classe
 
   def initialize(name, x, y, zlist, width, length)
     @name = name
@@ -10,53 +10,53 @@ class GameObject #qualquer objeto que irá existir precisa desses elementos de c
     @hitbox = Hitbox.new(x,y,zlist,width,length)
   end
 
-  def draw()
+  def draw() # Objeto base só mostra o sprite
     @sprite.draw(@x, @y, @z)
   end
 
-  def hit?(obj) #pergunta ao objeto se ele colide com determinada área tridimensional
-    return @hitbox.check_hit(obj.hitbox.x, obj.hitbox.y, obj.hitbox.zlist, obj.hitbox.width, obj.hitbox.length)
+  def hit?(obj) # Pergunta ao objeto se ele colide com determinada área tridimensional
+    return @hitbox.check_hit(obj.hitbox)
   end
 
   attr_reader :hitbox
 
-  def update_frame()
+  def update_frame() # Lista de instruções que cada objeto deve fazer(a ser definida dentro da classe de cada objeto)
   end
 
 end
 
-class Falcon < GameObject #objeto controlado pelo player. não necessariamente um passaro
+class Falcon < GameObject # Objeto controlado pelo player. não necessariamente um passaro
 
   def initialize(name, x, y, zlist, width, length)
-    @movCoolDownTimerV = 0 #evita aceleração descontrolada na vertical
-    @movCoolDownTimerH = 0 #evita aceleração descontrolada na horizontal
-    @name = name #nome define qual a imagem a ser carregada, preset falcon para passaro
-    @sprites = [Sprite.new(name + "1.png"), Sprite.new(name + "2.png"),Sprite.new(name + "3.png"),Sprite.new(name + "4.png"),Sprite.new(name + "5.png")] #todos os frames de animação são um sprite separado
-    @hitbox = Hitbox.new(x,y,zlist,width,length) #area ocupada pelo player
-    @spritestate = 1 #estado de frame de animação
-    @spriteStateCounter = 0 #contador para mudança de estado de frame de animação
+    @movCoolDownTimerV = 0 # Evita aceleração descontrolada na vertical
+    @movCoolDownTimerH = 0 # Evita aceleração descontrolada na horizontal
+    @name = name # Nome define qual a imagem a ser carregada, preset falcon para passaro
+    @sprites = [Sprite.new("./img/" + name + "/" + name + "1.png"), Sprite.new("./img/" + name + "/" + name + "2.png"),Sprite.new("./img/" + name + "/" + name + "3.png"),Sprite.new("./img/" + name + "/" + name + "4.png"),Sprite.new("./img/" + name + "/" + name + "5.png")] # Todos os frames de animação são um sprite separado
+    @hitbox = Hitbox.new(x,y,zlist,width,length) # Area ocupada pelo player
+    @spritestate = 1 # Estado de frame de animação
+    @spriteStateCounter = 0 # Contador para mudança de estado de frame de animação
   end
 
-  def draw() #objeto renderiza seu sprite e uma sombra do seu sprite, usando o sprite do frame de animação atual
+  def draw() # Objeto renderiza seu sprite e uma sombra do seu sprite, usando o sprite do frame de animação atual
     @sprites[@spritestate - 1].draw(@hitbox.x, @hitbox.y, @hitbox.zlist[0])
-    Gosu::Image.new(@name + @spritestate.to_s + ".png").draw(@hitbox.x, @hitbox.y, @hitbox.zlist[0]-1, 1, 1, 0xff_464678, :default)
+    Gosu::Image.new("./img/" + @name + "/" + @name + @spritestate.to_s + ".png").draw(@hitbox.x, @hitbox.y, @hitbox.zlist[0]-1, 1, 1, 0xff_464678, :default)
   end
 
-  def mov_up() #aumenta altura do objeto
+  def mov_up() # Aumenta altura do objeto
     if (@hitbox.zlist[0] < 35)
   	  @hitbox.zlist[0] = @hitbox.zlist[0] + 1
       @movCoolDownTimerV = 1
     end
   end
   
-  def mov_down() #abaixa altura do objeto
+  def mov_down() # Abaixa altura do objeto
     if (@hitbox.zlist[0] > 1)
     	@hitbox.zlist[0] = @hitbox.zlist[0] - 1
 	    @movCoolDownTimerV = 1
     end
   end
 
-  def mov_right() #movimento diagonal direita-baixo
+  def mov_right() # Movimento diagonal direita-baixo
     if (@hitbox.y + @hitbox.length < 480 - 32)
 	    @hitbox.y = @hitbox.y + 1
 	    @hitbox.x = @hitbox.x + 2
@@ -64,7 +64,7 @@ class Falcon < GameObject #objeto controlado pelo player. não necessariamente u
     end
   end
 
-  def mov_left() #movimento diagonal esquerda-cima
+  def mov_left() # Movimento diagonal esquerda-cima
     if (@hitbox.x > 32)
 	    @hitbox.y = @hitbox.y - 1
 	    @hitbox.x = @hitbox.x - 2
@@ -72,36 +72,36 @@ class Falcon < GameObject #objeto controlado pelo player. não necessariamente u
     end
   end
 
-  def update_frame() #instruções a serem executadas a cada frame do jogo, atualiza o estado de animação e recebe entrada do teclado
-    @spriteStateCounter = @spriteStateCounter + 1 #conta mais um frame no mesmo estado
-    if(@spriteStateCounter  ==  6) #se for o sexto frame
-      @spriteStateCounter = 0 #reseta o contador
-      @spritestate = @spritestate + 1 #e atualiza o estado
-      if(@spritestate  ==  6) #se o estado não existir
-        @spritestate = 1 #rebobine a animação
+  def update_frame() # Instruções a serem executadas a cada frame do jogo, atualiza o estado de animação e recebe entrada do teclado
+    @spriteStateCounter = @spriteStateCounter + 1 # Conta mais um frame no mesmo estado
+    if(@spriteStateCounter  ==  6) # Se for o sexto frame
+      @spriteStateCounter = 0 # Reseta o contador
+      @spritestate = @spritestate + 1 # E atualiza o estado
+      if(@spritestate  ==  6) # Se o estado não existir
+        @spritestate = 1 # Rebobine a animação
       end
     end
 
-    if @movCoolDownTimerV > 0 #contagem regressiva para movimento vertical
+    if @movCoolDownTimerV > 0 # Contagem regressiva para movimento vertical
       @movCoolDownTimerV = @movCoolDownTimerV - 1
     end
 
-    if @movCoolDownTimerH > 0 #contagem regressiva para movimento horizontal
+    if @movCoolDownTimerH > 0 # Contagem regressiva para movimento horizontal
       @movCoolDownTimerH = @movCoolDownTimerH - 1
     end
-    if ((Gosu.button_down? Gosu::KB_UP) and (@movCoolDownTimerV == 0)) #se jogador manda subir e não está em turno de espera
+    if ((Gosu.button_down? Gosu::KB_UP) and (@movCoolDownTimerV == 0)) # Se jogador manda subir e não está em turno de espera
       self.mov_up
     end
 
-    if ((Gosu.button_down? Gosu::KB_DOWN) and (@movCoolDownTimerV == 0)) #se jogador manda descer e não está em turno de espera
+    if ((Gosu.button_down? Gosu::KB_DOWN) and (@movCoolDownTimerV == 0)) # Se jogador manda descer e não está em turno de espera
       self.mov_down
     end
 
-    if ((Gosu.button_down? Gosu::KB_RIGHT) and (@movCoolDownTimerH == 0))#se jogador manda ir para a direita e não está em turno de espera
+    if ((Gosu.button_down? Gosu::KB_RIGHT) and (@movCoolDownTimerH == 0))# Se jogador manda ir para a direita e não está em turno de espera
       self.mov_right
     end
 
-    if ((Gosu.button_down? Gosu::KB_LEFT) and (@movCoolDownTimerH  ==  0))#se jogador manda ir para a esquerda e não está em turno de espera
+    if ((Gosu.button_down? Gosu::KB_LEFT) and (@movCoolDownTimerH  ==  0))# Se jogador manda ir para a esquerda e não está em turno de espera
       self.mov_left
     end
 
@@ -109,21 +109,21 @@ class Falcon < GameObject #objeto controlado pelo player. não necessariamente u
 
 end
 
-class Hiero < GameObject
+class Hiero < GameObject # Objeto objetivo do jogador, pode ser coletado pelo falcon, se move em direção ao canto inferior esquerdo
 
   def initialize(name, x, y, zlist, width, length)
-    @movCoolDownTimer = 0 #temporizador do movimento dos hieros
+    @movCoolDownTimer = 0 # Temporizador do movimento dos hieros
     @name = name
-    @sprite = Sprite.new(name + ".png") #imagem do hiero encapsulada em um sprite
-    @hitbox = Hitbox.new(x,y,zlist,width,length) #hitbox do hiero
+    @sprite = Sprite.new("./hieros/" + name + ".png") # Imagem do hiero encapsulada em um sprite
+    @hitbox = Hitbox.new(x,y,zlist,width,length) # Hitbox do hiero
   end
 
-  def draw() #objeto renderiza seu sprite e uma sombra do seu sprite
+  def draw() # Objeto renderiza seu sprite e uma sombra do seu sprite
     @sprite.draw(@hitbox.x, @hitbox.y, @hitbox.zlist[0])
-    Gosu::Image.new(@name + ".png").draw(@hitbox.x, @hitbox.y, @hitbox.zlist[0] - 1, 1, 1, 0xff_464678, :default)
+    Gosu::Image.new("./hieros/" + @name + ".png").draw(@hitbox.x, @hitbox.y, @hitbox.zlist[0] - 1, 1, 1, 0xff_464678, :default)
   end
 
-  def update_frame() #a cada frame, caso o temporizador esteja zerado, o hiero deve mover na diagonal para gerar impressão de movimento do personagem. o temporizador impede que ele se movimente rápido demais 
+  def update_frame() # A cada frame, caso o temporizador esteja zerado, o hiero deve mover na diagonal para gerar impressão de movimento do personagem. o temporizador impede que ele se movimente rápido demais 
     if @movCoolDownTimer > 0
       @movCoolDownTimer = @movCoolDownTimer - 1
     elsif(@movCoolDownTimer == 0)
@@ -135,20 +135,20 @@ class Hiero < GameObject
 
 end
 
-class Rock < GameObject
+class Rock < GameObject # Decoração pro fundo da tela
 
   def initialize(name, x, y)
-    @movCoolDownTimer = 0 #pedra se move com velocidade controlada
-    @name = name #cada nome gera uma pedra diferente
-    @sprite = Sprite.new(name + ".png") #imagem da pedra de determinado nome
-    @hitbox = Hitbox.new(x,y,[0],0,0) #pedra tem hitbox sempre no chão e sem largura, é só decorativa
+    @movCoolDownTimer = 0 # Pedra se move com velocidade controlada
+    @name = name # Cada nome gera uma pedra diferente
+    @sprite = Sprite.new("./rocks/" + name + ".png") # Imagem da pedra de determinado nome
+    @hitbox = Hitbox.new(x,y,[0],0,0) # Pedra tem hitbox sempre no chão e sem largura, é só decorativa
   end
 
-  def draw() #pedra sempre no chão, não precisa de sombra
+  def draw() # Pedra é printada sempre no chão, não precisa de sombra
     @sprite.draw(@hitbox.x, @hitbox.y, @hitbox.zlist[0])
   end
 
-  def update_frame() #pedra se move para gerar impressão de movimento do jogador
+  def update_frame() # Pedra se move para gerar impressão de movimento do jogador
     if @movCoolDownTimer > 0
       @movCoolDownTimer = @movCoolDownTimer - 1
     elsif(@movCoolDownTimer == 0)
